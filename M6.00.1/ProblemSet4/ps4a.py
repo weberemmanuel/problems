7f0567a92837c8c63b5db6ebb0b6c7c2eb776e05
Emmanuel Weber <eweber@bluejeans.com>
2017-06-27 14:56:34 -0700

    Complete game implementation

--- M6.00.1/ProblemSet4/ps4a.py
+++ M6.00.1/ProblemSet4/ps4a.py
@@ -264,15 +264,27 @@ def playGame(wordList):
 
     2) When done playing the hand, repeat from step 1
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
-
+    hand = None
+    while True:
+        option = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if option == 'e':
+            break;
+        elif option == 'n':
+            hand = dealHand(HAND_SIZE)
+        elif option == 'r':
+            if hand == None:
+                print('You have not played a hand yet. Please play a new hand first!')
+                continue
+        else:
+            print('Invalid command.')
+            continue
+        playHand(hand,wordList,HAND_SIZE)
 
 #wordList = loadWords()
 #playHand({'w':1, 's':1, 't':2, 'a':1, 'o':1, 'f':1}, wordList, 7)
 
-wordList = loadWords()
-playHand({'n':1, 'e':1, 't':1, 'a':1, 'r':1, 'i':2}, wordList, 7)
+#wordList = loadWords()
+#playHand({'n':1, 'e':1, 't':1, 'a':1, 'r':1, 'i':2}, wordList, 7)
 
 #
 # Build data structures used for entire session and play game
