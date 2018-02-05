a5dfbc6b9468c6e6d5ec18167970545ce8a40421
Emmanuel Weber <weberemmanuel@hotmail.com>
2018-02-05 14:07:16 -0800

    Loading adjmatrix

--- Algorithms/MinCut/src/MinCut.cpp
+++ Algorithms/MinCut/src/MinCut.cpp
@@ -7,9 +7,40 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <memory.h>
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int main(int argc, char** argv)
+{
+	if(argc < 2)
+	{
+		cout << "Usage: MinCut filename" << endl;
+		exit(0);
+	}
+	int mat[200][200];
+
+	memset(mat,0,sizeof(mat));
+
+	std::ifstream infile(argv[1]);
+
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		cout << line << endl;
+		std::istringstream iss(line);
+		int entry;
+		int index = -1;
+		while (iss >> entry)
+		{
+			if(index < 0)
+				index = entry;
+			else
+				mat[index][entry] = 1;
+		}
+	}
 	return 0;
 }
